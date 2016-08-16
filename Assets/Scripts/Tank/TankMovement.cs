ce3d3b7061c0a9af789d76943cc0fe33e7778f31
Arkadiusz Gac <arkadiusz.gac@gmail.com>
2016-08-16 14:10:15 +0200

    Tank that moves

---
+++ Assets/Scripts/Tank/TankMovement.cs
@@ -0,0 +1,106 @@
+﻿using UnityEngine;
+
+public class TankMovement : MonoBehaviour
+{
+    public int m_PlayerNumber = 1;         
+    public float m_Speed = 12f;            
+    public float m_TurnSpeed = 180f;       
+    public AudioSource m_MovementAudio;    
+    public AudioClip m_EngineIdling;       
+    public AudioClip m_EngineDriving;      
+    public float m_PitchRange = 0.2f;
+
+    private string m_MovementAxisName;     
+    private string m_TurnAxisName;         
+    private Rigidbody m_Rigidbody;         
+    private float m_MovementInputValue;    
+    private float m_TurnInputValue;        
+    private float m_OriginalPitch;         
+
+
+    private void Awake()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
+    }
+
+
+    private void OnEnable ()
+    {
+        m_Rigidbody.isKinematic = false;
+        m_MovementInputValue = 0f;
+        m_TurnInputValue = 0f;
+    }
+
+
+    private void OnDisable ()
+    {
+        m_Rigidbody.isKinematic = true;
+    }
+
+
+    private void Start()
+    {
+        m_MovementAxisName = "Vertical" + m_PlayerNumber;
+        m_TurnAxisName = "Horizontal" + m_PlayerNumber;
+
+        m_OriginalPitch = m_MovementAudio.pitch;
+    }
+
+    private void Update()
+    {
+		m_MovementInputValue = Input.GetAxis (m_MovementAxisName);
+		m_TurnInputValue = Input.GetAxis (m_TurnAxisName);
+
+		EngineAudio ();
+    }
+
+
+    private void EngineAudio()
+    {
+		if (IsTankMoving()) {
+			if (m_MovementAudio.clip == m_EngineDriving) {
+				SwitchClipAndPlay (m_EngineIdling);
+			}
+		} else {
+			if (m_MovementAudio.clip == m_EngineIdling) {
+				SwitchClipAndPlay (m_EngineDriving);
+			}
+		}
+    }
+
+	private bool IsTankMoving()
+	{
+		return (Mathf.Abs (m_MovementInputValue) < 0.1f && Mathf.Abs (m_TurnInputValue) < 0.1f);
+	}
+
+	private void SwitchClipAndPlay(AudioClip clip)
+	{
+		m_MovementAudio.clip = clip;
+		m_MovementAudio.pitch = Random.Range (m_OriginalPitch - m_PitchRange, m_OriginalPitch + m_PitchRange);
+		m_MovementAudio.Play ();
+	}
+
+
+    private void FixedUpdate()
+    {
+		Turn ();
+//		if (m_TurnInputValue == 0) {
+		Move ();
+//		}
+    }
+
+
+    private void Move()
+    {
+		Vector3 movement = transform.forward * m_MovementInputValue * m_Speed * Time.deltaTime;
+		m_Rigidbody.MovePosition (m_Rigidbody.position + movement);
+    }
+
+
+    private void Turn()
+    {
+		float turn = m_TurnInputValue * m_TurnSpeed * Time.deltaTime;
+		Quaternion turnRotation = Quaternion.Euler (0f, turn, 0f);
+		m_Rigidbody.MoveRotation (m_Rigidbody.rotation * turnRotation);
+    }
+}
