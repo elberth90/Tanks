3aa5886e2577b51b6ae3c10b45d2d465ef28b306
elberth90 <arkadiusz.gac@gmail.com>
2016-08-18 21:14:40 +0200

    Add health

--- Assets/Scripts/Tank/TankHealth.cs
+++ Assets/Scripts/Tank/TankHealth.cs
@@ -10,7 +10,6 @@ public class TankHealth : MonoBehaviour
     public Color m_ZeroHealthColor = Color.red;    
     public GameObject m_ExplosionPrefab;
     
-    /*
     private AudioSource m_ExplosionAudio;          
     private ParticleSystem m_ExplosionParticles;   
     private float m_CurrentHealth;  
@@ -33,22 +32,33 @@ public class TankHealth : MonoBehaviour
 
         SetHealthUI();
     }
-    */
 
     public void TakeDamage(float amount)
     {
-        // Adjust the tank's current health, update the UI based on the new health and check whether or not the tank is dead.
+		this.m_CurrentHealth -= amount;
+		this.SetHealthUI ();
+		if (this.m_CurrentHealth <= 0f && !this.m_Dead) {
+			this.OnDeath ();
+		}
     }
 
 
     private void SetHealthUI()
     {
-        // Adjust the value and colour of the slider.
+		this.m_Slider.value = this.m_CurrentHealth;
+		this.m_FillImage.color = Color.Lerp (this.m_ZeroHealthColor, this.m_FullHealthColor, this.m_CurrentHealth / this.m_StartingHealth);
     }
 
 
     private void OnDeath()
     {
-        // Play the effects for the death of the tank and deactivate it.
+		this.m_Dead = true;
+
+		this.m_ExplosionParticles.transform.position = transform.position;
+		this.m_ExplosionParticles.gameObject.SetActive (true);
+		this.m_ExplosionParticles.Play ();
+		this.m_ExplosionAudio.Play ();
+
+		gameObject.SetActive (false);
     }
 }
